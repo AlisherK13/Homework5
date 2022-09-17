823fd33dc1b6736ea55f7c5889951ff4ae124359
Alisher <kusainov-alisher@mail.ru>
2022-09-17 21:09:54 +0600

    added Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу копирования массива
+
+int[] mass = new int[5];
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void CopyArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+FillArray(mass);
+PrintArray(mass);
+CopyArray(mass);
